6ff5f2ac55768ef59534d4aa8c2037c759c9678b
aletorres73 <torresalejandro7392@gmail.com>
2025-01-27 15:38:18 -0300

    version name: 1.0-alpha02

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -70,7 +70,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "1.0-alpha01"
+        versionName = "1.0-alpha02"
     }
     packaging {
         resources {
