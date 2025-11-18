36a71e280d43606788c3c3fa90dfff4ada3ef6d4
aletorres73 <torresalejandro7392@gmail.com>
2025-11-18 14:10:03 -0300

    version name: 3.0.0.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -84,7 +84,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "3.0.0.1"
     }
     packaging {
         resources {
