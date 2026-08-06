fb36796175380b5a1b76229969862070aacf39cc
aletorres73 <torresalejandro7392@gmail.com>
2026-08-06 15:34:52 -0300

    Increment app version to 4.3.1
    - Updated `versionName` to 4.3.1 in `composeApp/build.gradle.kts`.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -96,7 +96,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "4.3.0"
+        versionName = "4.3.1"
     }
     buildFeatures {
         compose = true
